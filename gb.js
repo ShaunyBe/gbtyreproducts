0ceac05b52387597659b2ceaf1cb22947da2200a
Shaun Beukman <80125125+ShaunyBe@users.noreply.github.com>
2021-04-20 14:21:29 +0100

    Add files via upload

---
+++ gb.js
@@ -0,0 +1,15 @@
+/* Back to top button*/
+
+
+
+
+
+/*Responsive Navbar */
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "navbar") {
+      x.className += " responsive";
+    } else {
+      x.className = "navbar";
+    }
+  }
